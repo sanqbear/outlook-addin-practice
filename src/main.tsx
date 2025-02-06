b4cad8e2fa6894e047ab9afe72351c0841c2bd6f
Sanqbear <sanqbear@outlook.com>
2025-02-06 16:10:03 +0900

    add: office add-in integration

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { FluentProvider, teamsLightTheme } from "@fluentui/react-components";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+const bodyRoot = document.getElementById("root");
+if (bodyRoot) {
+  createRoot(bodyRoot).render(
+    <StrictMode>
+      <FluentProvider theme={teamsLightTheme}>
+        <App />
+      </FluentProvider>
+    </StrictMode>
+  );
+}
