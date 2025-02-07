17624875d359e6ec6380be02c4eddfec19efc518
sanqbear <sanqbear@outlook.com>
2025-02-07 17:41:31 +0900

    update: current research in-progress results

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,45 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import mkcert from 'vite-plugin-mkcert';
+import devCerts from "office-addin-dev-certs";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), mkcert()],
-  server: {
-    port: 3000,
-  },
-});
+async function getHttpsOptions() {
+  const httpsOptions = await devCerts.getHttpsServerOptions();
+  return {
+    ca: httpsOptions.ca,
+    cert: httpsOptions.cert,
+    key: httpsOptions.key,
+  };
+}
+
+async function getConfig() {
+  const httpsOptions = await getHttpsOptions();
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      port: 3000,
+      https: {
+        key: httpsOptions.key,
+        cert: httpsOptions.cert,
+      },
+      cors: true,
+      proxy: {
+        "/officeaddin": {
+          target: "https://localhost:7287",
+          changeOrigin: true,
+          secure: false,
+        },
+        "/resources": {
+          target: "https://localhost:7287",
+          changeOrigin: true,
+          secure: false,
+        },
+
+      },
+
+    },
+  });
+}
+
+const config = await getConfig();
+
+export default config;
